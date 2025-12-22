0c949862410ffbed673e3abd4e2e9f4ad2045f7e
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2025-12-22 18:31:15 +0530

    secuirty updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,57 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY", // prevents clickjacking
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "Permissions-Policy",
+    value:
+      "camera=(), microphone=(), geolocation=(), payment=(), usb=()",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+];
+
 const nextConfig = {
-    images: {
+  reactStrictMode: true,
+
+  images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "api.shopsmaart.com",
-        pathname: "**",
+        pathname: "/**",
       },
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
--- src/components/helper/Helper.js
+++ src/components/helper/Helper.js
@@ -1,5 +1,10 @@
 export const BASE_URL = "https://api.shopsmaart.com"
 
 
+
+const url = process.env.NEXT_PUBLIC_BASE_URLL;
+
+// console.log("url is ", url)
+
 // use this base url for the testing purpose;
-export const base_url = "https://api.shopsmaart.com";
+export const base_url = url

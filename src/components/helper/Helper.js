253120b567837aaa480f6939e19216efa7b6546d
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2025-12-23 10:53:25 +0530

    base url updated

--- src/components/helper/Helper.js
+++ src/components/helper/Helper.js
@@ -1,10 +1,7 @@
-export const BASE_URL = "https://api.shopsmaart.com"
-
-
 
 const url = process.env.NEXT_PUBLIC_BASE_URLL;
 
 // console.log("url is ", url)
-
+export const BASE_URL = url;
 // use this base url for the testing purpose;
-export const base_url = url
+export const base_url = url;
